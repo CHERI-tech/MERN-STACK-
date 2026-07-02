6cbe2651140fdba737bb699b7be0fcd1b887eaaf
CHERI-tech <albymanoj30@gmail.com>
2026-07-02 19:11:41 +0530

    Initialize project and add Day 1 folder

---
+++ DAY 1/Java Script/script.js
@@ -0,0 +1,44 @@
+console.log("Welcome to JavaScript!");
+document.write("Hello CEK");
+// This is a single-line comment
+/* This is a multi-line comment
+   that spans multiple lines */
+let name="Alby Manoj";
+console.log(name);   
+console.log(typeof(name));
+let salary=50000;
+console.log(salary);
+console.log(typeof(salary));
+var invalid=false;
+console.log(invalid);
+console.log(typeof(invalid));
+const pi=3.14;
+console.log(pi);
+console.log(typeof(pi));
+let MyAge
+console.log(MyAge);
+sum=1+3;
+console.log(sum);
+console.log(typeof(sum));
+diff=1-3;
+console.log(diff);
+console.log(typeof(diff));
+div=4/2;
+console.log(div);
+console.log(typeof(div));
+mul=2*3;
+console.log(mul);
+console.log(typeof(mul));
+exponent=2**3;
+console.log(exponent);
+console.log(typeof(exponent));
+let x=5;
+x+=5;
+console.log(x);
+x-=2;
+console.log(x);
+let flag1=true;
+let flag2=false;
+console.log(flag1==flag2); //variable comparison
+console.log(flag1===flag2);//value and type
+
